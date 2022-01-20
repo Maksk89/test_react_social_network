7c30643257a6cebf4e2292d2606653fbdb73c393
Maksim Kormiltsev <newarty28@gmail.com>
2022-01-20 22:35:29 +0300

    Add UsersFunctionalComponent

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Users from './UsersC';
 import {
     followActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator,
     setUsersActionCreator,
     unfollowActionCreator
 } from '../../my_redux/usersReducer';
+import axios from 'axios';
+import UsersFunctionalComponent from './UsersFunctionalComponent';
+
+class UsersAPIComponent extends Component {
+
+    componentDidMount () {
+        if (this.props.users.length === 0) {
+            axios.get (`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+                .then (response => {
+
+                    this.props.setUsers (response.data.items);
+                    this.props.setTotalUsersCount (response.data.totalCount);
+                });
+        }
+    }
+
+    pageChangeHandler = (pageNumber) => {
+        this.props.setCurrentPage (pageNumber);
+        axios.get (`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then (response => {
+                this.props.setUsers (response.data.items);
+
+            });
+    };
+
+    render () {
+        return <UsersFunctionalComponent totalUsersCount = {this.props.totalUsersCount}
+                                         pageSize = {this.props.pageSize}
+                                         currentPage = {this.props.currentPage}
+                                         pageChangeHandler = {this.pageChangeHandler}
+                                         users = {this.props.users}
+                                         follow = {this.props.follow}
+                                         unfollow = {this.props.unfollow}
+        />;
+    }
+}
 
 
 const mapStateToProps = (state) => {
@@ -39,5 +74,5 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 
-const UsersContainer = connect (mapStateToProps, mapDispatchToProps) (Users);
+const UsersContainer = connect (mapStateToProps, mapDispatchToProps) (UsersAPIComponent);
 export default UsersContainer;
---
+++ src/components/Users/UsersFunctionalComponent.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './Users.module.css';
+import userPhoto from '../../assets/images/user.png';
+
+function UsersFunctionalComponent (props) {
+    const pagesCount = Math.ceil (props.totalUsersCount / props.pageSize);
+    const pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push (i);
+    }
+    return (
+        <div>
+            <div>
+                {pages.map (page => {
+                    return <span
+                        className = {props.currentPage === page && styles.selectedPage}
+                        onClick = {(event) => {
+                            props.pageChangeHandler (page);
+                        }}>{page}</span>;
+                })}
+            </div>
+            <div>
+                {
+                    props.users.map (user => <div key = {user.id}>
+                        <span>
+                            <div>
+                            <img src = {user.photos.small != null ? user.photos.small : userPhoto}
+                                 alt = ''
+                                 className = {styles.userPhoto} />
+                        </div>
+                            <div>
+                                {
+                                    user.followed
+                                        ? <button onClick = {() => {
+                                            props.unfollow (user.id);
+                                        }}>Unfollow</button>
+                                        : <button onClick = {() => {
+                                            props.follow (user.id);
+                                        }}>Follow</button>
+                                }
+                            </div>
+                        </span>
+                            <span>
+                            <span>
+                                <div>
+                                    {user.name}
+                                </div>
+                                <div>
+                                    {user.status}
+                                </div>
+                            </span>
+                            <span>
+                                <div>{"user.location.city"}</div>
+                                <div>{"user.location.country"}</div>
+                        </span>
+                        </span>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
+}
+
+export default UsersFunctionalComponent;
